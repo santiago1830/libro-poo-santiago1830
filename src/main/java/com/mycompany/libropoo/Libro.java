461d3c99894b4e3808c95d1aacdfe9d4022c605b
santiago1830 <146890780+santiago1830@users.noreply.github.com>
2024-06-04 09:59:23 -0500

    Update Libro.java
    ...

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,38 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+   
+    private String titulo;
+    private String autor;
+    private int anoPublicacion;
+    private String genero; 
+    private boolean leido;
+
+
+    public Libro(String titulo, String autor, int anoPublicacion, String genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacion = anoPublicacion;
+        this.genero = genero; 
+        this.leido = false; 
+    }
+
     
+    public String mostrarInformacion() {
+        return "Título: " + titulo + ", Autor: " + autor + ", Año de publicación: " + anoPublicacion + ", Género: " + genero;
+    }
+
+    public void marcarLeido() {
+        this.leido = true;
+    }
+
+    public boolean esAntiguo() {
+        int currentYear = java.time.Year.now().getValue();
+        return currentYear - anoPublicacion > 50;
+    }
+
+   
+    public boolean isLeido() {
+        return leido;
+    }
 }
