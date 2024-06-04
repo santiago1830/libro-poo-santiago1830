ba095cbb1b19cee8ed446078d961323f3642bc2b
santiago1830 <146890780+santiago1830@users.noreply.github.com>
2024-06-04 10:11:48 -0500

    Update Principal.java

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,41 @@
 package com.mycompany.libropoo;
 
-public class Principal {
+import java.util.Scanner;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
     
+        System.out.println("Ingrese el título del libro:");
+        String titulo = scanner.nextLine();
+
+        System.out.println("Ingrese el autor del libro:");
+        String autor = scanner.nextLine();
+
+        System.out.println("Ingrese el año de publicación del libro:");
+        int anoPublicacion = scanner.nextInt();
+        scanner.nextLine(); // Consumir la nueva línea
+
+        System.out.println("Ingrese el género del libro:");
+        String genero = scanner.nextLine();
+
+       
+        Libro libro = new Libro(titulo, autor, anoPublicacion, genero);
+
+       
+        libro.marcarLeido();
+
+       
+        System.out.println(libro.mostrarInformacion());
+
+       
+        System.out.println("¿El libro ya fue leído?: " + (libro.isLeido() ? "Sí" : "No"));
+
+       
+        System.out.println("¿El libro es antiguo?: " + (libro.esAntiguo() ? "Sí" : "No"));
+
+        scanner.close();
+    }
 }
+
